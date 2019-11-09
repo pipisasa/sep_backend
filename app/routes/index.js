6f63dc2798986aa7503d606f9d5ec8e4e3c0c052
pipisasa <emir0725mk@gmail.com>
2019-11-09 17:23:12 +0600

    orders api repatch

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 const dbRoutes = require('./dbRoutes');
-const CATsRoutes = require('./CATsRoutes')
+const CATsRoutes = require('./CATsRoutes');
+const orderRoutes = require('./orderRoutes')
 module.exports = function(app, db) {
   dbRoutes(app, db);
-  CATsRoutes(app, db)
+  CATsRoutes(app, db);
+  orderRoutes(app, db);
 };
